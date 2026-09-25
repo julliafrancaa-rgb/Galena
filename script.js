39b4b64881db7d93c95d034e646a8283621fb662
julliafrancaa-rgb <julliafrancaa@gmail.com>
2026-09-24 23:21:37 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function abrirMenu() {
 
+    const menu = document.getElementById("menu");
+
+    menu.classList.toggle("menu-aberto");
+
+}
+
+
+const linksMenu = document.querySelectorAll("#menu a");
+
+linksMenu.forEach(function(link) {
+
+    link.addEventListener("click", function() {
+
+        document.getElementById("menu").classList.remove("menu-aberto");
+
+    });
+
+});
